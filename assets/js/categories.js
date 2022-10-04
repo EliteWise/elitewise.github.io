b92b25216a3792d33d56f9df4acbb9340ff547f3
Florent <43142477+EliteWise@users.noreply.github.com>
2022-10-04 15:04:01 +0200

    Update categories.js

--- assets/js/categories.js
+++ assets/js/categories.js
@@ -5,34 +5,6 @@ const categories = { {% for category in site.categories %}{% capture category_na
 
 console.log(categories)
 
-window.onload = function () {
-  document.querySelectorAll(".category").forEach((category) => {
-    category.addEventListener("click", function (e) {
-      const posts = categories[e.target.innerText.replace(" ","_")];
-      let html = ``
-      posts.forEach(post=>{
-        html += `
-        <a class="modal-article" href="${post.url}">
-          <h4>${post.title}</h4>
-          <small class="modal-article-date">${post.date}</small>
-        </a>
-        `
-      })
-      document.querySelector("#category-modal-title").innerText = e.target.innerText;
-      document.querySelector("#category-modal-content").innerHTML = html;
-      document.querySelector("#category-modal-bg").classList.toggle("open");
-      document.querySelector("#category-modal").classList.toggle("open");
-    });
-  });
-
-  document.querySelector("#category-modal-bg").addEventListener("click", function(){
-    document.querySelector("#category-modal-title").innerText = "";
-    document.querySelector("#category-modal-content").innerHTML = "";
-    document.querySelector("#category-modal-bg").classList.toggle("open");
-    document.querySelector("#category-modal").classList.toggle("open");
-  })
-};
-
 function postRedirect(element) {
    var link = element.getElementsByTagName('a')[0].href;
    window.location.replace(link);
